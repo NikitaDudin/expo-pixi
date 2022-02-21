f0993bfb9458bd4ca11ddad4b901cf2a0be12ac0
Nikita Dudin <nikita.dudin@gmail.com>
2022-02-21 10:34:01 +0300

    Add typescript declarations

---
+++ lib/index.d.ts
@@ -0,0 +1,17 @@
+import * as PIXI from './pixi.d';
+
+type Signature = any;
+type Sketch = any;
+type FilterImage = any;
+declare function spineAsync(...args: any[]): void;
+
+export {
+  PIXI,
+  // TODO: add types for below components
+  Signature,
+  Sketch,
+  FilterImage,
+  spineAsync,
+};
+
+export default {};
